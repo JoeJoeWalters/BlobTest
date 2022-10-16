9bf2e51b1da7df3cfcb10791f9a4b5b1aec65ab9
Joe Walters <info@thenakeddeveloper.com>
2022-10-17 00:33:59 +0100

    en_GB Locale

--- src/Feed/Program.cs
+++ src/Feed/Program.cs
@@ -1,19 +1,18 @@
 ﻿using Common;
 using Bogus;
 using Bogus.DataSets;
-using Bogus.Extensions;
+using Bogus.Extensions.UnitedKingdom;
 using static Bogus.DataSets.Name;
 
-var fakeAddress = new Faker<Common.Address>()
+var fakeAddress = new Faker<Common.Address>("en_GB")
     .RuleFor(a => a.AddressLine1, (f, a) => f.Address.StreetAddress())
     .RuleFor(a => a.AddressLine2, (f, a) => f.Address.SecondaryAddress())
     .RuleFor(a => a.BuildingNumber, (f, a) => f.Address.BuildingNumber())
     .RuleFor(a => a.City, (f, a) => f.Address.City())
-    .RuleFor(a => a.State, (f, a) => f.Address.State())
-    .RuleFor(a => a.PostalCode, (f, a) => f.Address.ZipCode())
-    .RuleFor(a => a.CountryCode, (f, a) => f.Address.CountryCode());
+    .RuleFor(a => a.State, (f, a) => f.Address.County())
+    .RuleFor(a => a.PostalCode, (f, a) => f.Address.ZipCode());
 
-var fakePerson = new Faker<Common.Person>()
+var fakePerson = new Faker<Common.Person>("en_GB")
     .RuleFor(p => p.Gender, f => f.PickRandom<Common.Gender>())
     .RuleFor(p => p.Name, (f, u) => f.Name.FirstName((Name.Gender)u.Gender))
     .RuleFor(p => p.Surname, (f, u) => f.Name.LastName((Name.Gender)u.Gender))
