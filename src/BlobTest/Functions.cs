e4cd3b8c6d770573ff125ceb8087901cfb3d954a
Joe Walters <info@thenakeddeveloper.com>
2022-10-17 23:46:15 +0100

    Outbox / inbox patterns

--- src/BlobTest/Functions.cs
+++ src/BlobTest/Functions.cs
@@ -7,34 +7,116 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Azure.Storage.Queues; // Namespace for Queue storage types
+using Azure.Storage.Queues.Models; // Namespace for PeekedMessage
+using System.Configuration; // Namespace for ConfigurationManager
+using System;
 
 namespace BlobTest
 {
     public class Functions
     {
         [FunctionName("HttpEndpoint")]
-        public static async Task<HttpResponseMessage> HttpEndpoint(
+        public async Task<HttpResponseMessage> HttpEndpoint(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "person")]
             HttpRequest req,
             ILogger log)
         {
             var content = await new StreamReader(req.Body).ReadToEndAsync();
 
-            Common.Person myClass = JsonConvert.DeserializeObject<Common.Person>(content);
+            Common.Person person = JsonConvert.DeserializeObject<Common.Person>(content);
+
+            string messageContent = JsonConvert.SerializeObject(person);
+
+            InsertMessage("reporting", messageContent);
 
             return new HttpResponseMessage() { StatusCode = HttpStatusCode.OK };
         }
 
         [FunctionName("BlobQueue")]
-        public static void BlobQueue([QueueTrigger("reporting", Connection = "")]string myQueueItem, ILogger log)
+        public void BlobQueue([QueueTrigger("reporting", Connection = "AzureWebJobsStorage")]string message, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            log.LogInformation($"Processing For Blob: {message}");
+
+            // Write the message to the next queue
+            InsertMessage("remotes", message);
+
+            log.LogInformation($"Message written to next queue");
         }
 
         [FunctionName("RemoteQueue")]
-        public static void RemoteQueue([QueueTrigger("reporting", Connection = "")] string myQueueItem, ILogger log)
+        public void RemoteQueue([QueueTrigger("remotes", Connection = "AzureWebJobsStorage")] string message, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            log.LogInformation($"Processing For Remote: {message}");
+
+            log.LogInformation($"Message completed");
+        }
+
+        public void CreateQueueClient(string queueName)
+        {
+            // Get the connection string from app settings
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            // Instantiate a QueueClient which will be used to create and manipulate the queue
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+        }
+
+        public bool CreateQueue(string queueName)
+        {
+            try
+            {
+                // Get the connection string from app settings
+                string connectionString = "UseDevelopmentStorage=true"; // ConfigurationManager.AppSettings["StorageConnectionString"];
+
+                // Instantiate a QueueClient which will be used to create and manipulate the queue
+                QueueClient queueClient = new QueueClient(connectionString, queueName, new QueueClientOptions
+                {
+                    MessageEncoding = QueueMessageEncoding.Base64
+                });
+
+                // Create the queue
+                queueClient.CreateIfNotExists();
+
+                if (queueClient.Exists())
+                {
+                    Console.WriteLine($"Queue created: '{queueClient.Name}'");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}\n\n");
+                Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
+                return false;
+            }
+        }
+
+        public void InsertMessage(string queueName, string message)
+        {
+            // Get the connection string from app settings
+            string connectionString = "UseDevelopmentStorage=true"; // ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            // Instantiate a QueueClient which will be used to create and manipulate the queue
+            QueueClient queueClient = new QueueClient(connectionString, queueName, new QueueClientOptions
+            {
+                MessageEncoding = QueueMessageEncoding.Base64
+            });
+
+            // Create the queue if it doesn't already exist
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+            {
+                // Send a message to the queue
+                queueClient.SendMessage(message);
+            }
+
+            Console.WriteLine($"Inserted: {message}");
         }
     }
 }
