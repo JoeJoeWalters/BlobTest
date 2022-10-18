f4cc89a067653b9bffd7a69bb5e77880c938bb24
Joe Walters <info@thenakeddeveloper.com>
2022-10-18 08:37:33 +0100

    Tidy Up

--- src/BlobTest/Functions.cs
+++ src/BlobTest/Functions.cs
@@ -16,6 +16,8 @@ namespace BlobTest
 {
     public class Functions
     {
+        private string _ConnectionString = "UseDevelopmentStorage=true";
+
         [FunctionName("HttpEndpoint")]
         public async Task<HttpResponseMessage> HttpEndpoint(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "person")]
@@ -52,57 +54,10 @@ namespace BlobTest
             log.LogInformation($"Message completed");
         }
 
-        public void CreateQueueClient(string queueName)
-        {
-            // Get the connection string from app settings
-            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
-
-            // Instantiate a QueueClient which will be used to create and manipulate the queue
-            QueueClient queueClient = new QueueClient(connectionString, queueName);
-        }
-
-        public bool CreateQueue(string queueName)
-        {
-            try
-            {
-                // Get the connection string from app settings
-                string connectionString = "UseDevelopmentStorage=true"; // ConfigurationManager.AppSettings["StorageConnectionString"];
-
-                // Instantiate a QueueClient which will be used to create and manipulate the queue
-                QueueClient queueClient = new QueueClient(connectionString, queueName, new QueueClientOptions
-                {
-                    MessageEncoding = QueueMessageEncoding.Base64
-                });
-
-                // Create the queue
-                queueClient.CreateIfNotExists();
-
-                if (queueClient.Exists())
-                {
-                    Console.WriteLine($"Queue created: '{queueClient.Name}'");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}\n\n");
-                Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
-                return false;
-            }
-        }
-
         public void InsertMessage(string queueName, string message)
         {
-            // Get the connection string from app settings
-            string connectionString = "UseDevelopmentStorage=true"; // ConfigurationManager.AppSettings["StorageConnectionString"];
-
             // Instantiate a QueueClient which will be used to create and manipulate the queue
-            QueueClient queueClient = new QueueClient(connectionString, queueName, new QueueClientOptions
+            QueueClient queueClient = new QueueClient(_ConnectionString, queueName, new QueueClientOptions
             {
                 MessageEncoding = QueueMessageEncoding.Base64
             });
